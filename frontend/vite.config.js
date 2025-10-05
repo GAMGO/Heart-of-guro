c5b37a973d3389e25d6b96ce175b78e7bf80d4c2
GAMGO <gamgo445@gmail.com>
2025-10-05 21:34:38 +0900

    bash

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const REPO_NAME = '/buzz-joe-project/';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? REPO_NAME : '/',
 })
